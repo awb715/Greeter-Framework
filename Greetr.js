c1ef0c0f547a94dc5faae2ac6187ec65e2148df1
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-23 20:50:02 -0700

    Chainable methods

--- Greetr.js
+++ Greetr.js
@@ -1,28 +1,74 @@
-(function (global,$) {
+(function (global, $) {
     //Immediately invoked function
-    
-    
-    var Greetr= function (firstName, lastName, language){
-         
+    var Greetr = function (firstName, lastName, language) {
         return new Greetr.init(firstName, lastName, language);
     }
-    
-    Greetr.prototype = { };
-    
-    Greetr.init = function (firstName, lastName, language){
+    var supportedLangs = ['en', 'es'];
+    var greetings = {
+        en: 'Hello'
+        , es: 'Hola'
+    };
+    var logMessages = {
+        en: 'Logged in'
+        , es: 'Inicio sesion'
+    };
+    var formalGreetings = {
+        en: 'Greetings'
+        , es: 'Saludos'
+    };
+    Greetr.prototype = {
+        fullName: function () {
+            return this.firstName + " " + this.lastName
+        }
+        , validate: function () {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        }
+        , greeting: function () {
+            return greetings[this.language] + this.firstName + "!";
+        }
+        , formalGreeting: function () {
+            return formalGreetings[this.language] + ',' + this.fullName;
+        }
+        , greet: function (formal) {
+            var msg;
+            //if undefined or null, the greeting will be standard
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+            if (console) {
+                console.log(msg);
+            }
+            // this refers to the object that calls greet
+            return this;
+        }
+        , log: function () {
+            if (console) {
+                console.log(logMessages[this.language] + ":" + this.fullName()) ;
+            }
+            return this; // Key point and lesson learned! : returning this allows you to chain methods so the full object is returned after changes in each method call!
+        },
+        setLang:function(lang){
+            this.language = lang;
+            this.validate();
+            
+            return this;
+        }
         
-        var self = this;
-        self.firstName = firstName || " ";
-        self.lastName = lastName || " ";
-        self.language = language || "en";
-        
-    }
-    //points any object created with the constructor to Greetr's prototype
-    Greetr.init.prototype =Greetr.prototype;
+    };
     
+    Greetr.init = function (firstName, lastName, language) {
+            var self = this;
+            self.firstName = firstName || " ";
+            self.lastName = lastName || " ";
+            self.language = language || "en";
+        }
+        //points any object created with the constructor to Greetr's prototype
+    Greetr.init.prototype = Greetr.prototype;
     //allows Greetr to be accessed globally
     global.Greetr = global.G$ = Greetr;
-    
-    
-    
 }(window, jQuery));
