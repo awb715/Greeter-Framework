2589baf4eae420a7747d9e8292d26c566c11ce8d
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-23 18:32:06 -0700

    Constructor

--- Greetr.js
+++ Greetr.js
@@ -2,6 +2,23 @@
     //Immediately invoked function
     
     
+    var Greetr= function (firstName, lastName, language){
+         
+        return new Greetr.init(firstName, lastName, language);
+    }
+    
+    Greetr.prototype = { };
+    
+    Greetr.init = function (firstName, lastName, language){
+        
+        var self = this;
+        self.firstName = firstName || " ";
+        self.lastName = lastName || " ";
+        self.language = language || "en";
+        
+    }
+    //points any object created with the constructor to Greetr's prototype
+    Greetr.init.prototype =Greetr.prototype;
     
     
     
